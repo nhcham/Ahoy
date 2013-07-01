304a14e794641dba70c5bc37993cdf886e2dea64
N. Harold Cham <n.harold.cham@gmail.com>
2013-07-01 11:47:25 +0200

    release script: append version number to apk package

--- release.sh
+++ release.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+VERSION_NUMBER=`grep -o 'android:versionName="[^"]*"' AndroidManifest.xml | sed s/android:versionName=//g | sed s/\"//g`
+echo $VERSION_NUMBER
 ant clean release
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.ssh/nhcham.keystore bin/Ahoy-unsigned.apk nhcham
-zipalign -v 4 bin/Ahoy-unsigned.apk bin/Ahoy-release.apk
+zipalign -v 4 bin/Ahoy-unsigned.apk bin/Ahoy-$VERSION_NUMBER.apk
